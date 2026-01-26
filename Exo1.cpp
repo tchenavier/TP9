0808fa504f1e55710de750e7fdf360d2a50d8ff8
tchenavier <thibaud.chenavier@gmail.com>
2026-01-26 17:44:50 +0100

    Ajoutez des fichiers projet.

---
+++ Exo1.cpp
@@ -0,0 +1,29 @@
+/*#include <stdio.h>
+
+typedef struct //On définit un type struct
+{
+	char nom[10];
+	char prenom[10];
+	int age;
+	float note;
+}
+fiche;
+
+int main(int argc, char** argv)
+{
+	fiche f1 = { "DUPONT", "Ari",25 ,5.5 }, f2;
+	//strcpy(f2.nom, "Duchamp");
+	//strcpy (f2.prenom, "Louis");
+	//f2.age = 20;
+	//f2.note = 12.75;
+
+
+
+	printf_s("%s\n", f1.nom);
+	printf_s("%s\n", f1.prenom);
+	printf_s("%d\n", f1.age);
+	printf_s("%f\n", f1.note);
+
+	printf_s("FIN");
+return 0;
+}*/
---
+++ Exo2.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+typedef struct // On définit un type struct
+{
+	int X[15];
+	int Y[15];
+}
+coordonnees;
+
+void affiche(coordonnees* X)
+{
+	for (int i = 0; i < 15; i++)
+	{
+		printf_s("Coordonee de X\n");
+		printf_s("%d\n", X->X[i]);
+		printf_s("Coordonee de Y\n");
+		printf_s("%d\n", X->Y[i]);
+	}
+}
+
+void Saisir(coordonnees* X)
+{
+	printf_s("Entree les valeur\n");
+	for (int i = 0; i < 15; i++)
+	{
+		scanf_s("%d", &X->X[i]);
+		scanf_s("%d",& X->Y[i]);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	coordonnees C;
+	coordonnees *C1;
+	C1 = &C;
+
+	Saisir(C1);
+	affiche(C1);
+
+	printf_s("FIN\n");
+	return 0;
+}
