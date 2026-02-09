cd3b6e46c460b980111b5b613d5fa32f96d33930
tchenavier <thibaud.chenavier@gmail.com>
2026-02-09 17:36:44 +0100

    simplification en cour(pas opérationelle)

--- Exo4.cpp
+++ Exo4.cpp
@@ -3,12 +3,19 @@
 
 typedef struct /* On définit un type struct */
 {
-	char nom[10];
-	char prenom[10];
+	char nom[15];
+	char prenom[15];
 	int nbnotes; //permet de stocker le nombre de notes saisies
 	float note[50];
 }
 noteseninfo;
+
+void viderBuffer(void)
+{
+	char c;
+	// Lire et ignorer tous les caractères jusqu'à la fin de ligne ou EOF
+	while ((c = getchar()) != '\n' && c != EOF) {}
+}
 
 void affiche(noteseninfo*fiche,int index)
 {
@@ -26,45 +33,52 @@ void affiche(noteseninfo*fiche,int index)
 
 int Saisir(noteseninfo* fiche, int* index)
 {
-	int nombre, total=0, reponse=0, j,i;
-	char nom[10], prenom[10];
+	int nombre, total=0, reponse=0,eleve,nouveauEleve;
+	char nom[15], prenom[15];
 
-	if (*index == 100 || *index > 100)
-	{
-		return -1;
-	}
-	else
-	{
-		printf_s("Pour quelle Eleve ? \n");
+	
+		viderBuffer();
+		printf_s("Pour quelle Eleve. \n");
 		printf_s("Quelle nom ?\n");
-		fgets(nom, 9, stdin);
+		fgets(nom, 14, stdin);
 		printf_s("Quelle prenom ? \n");
-		fgets(prenom, 9, stdin);
-		for (i = 0; i < 100; i++)
+		fgets(prenom, 14, stdin);
+
+		for (int i = 0; i < 100; i++)
 		{
 			if (nom == fiche[i].nom && prenom == fiche[i].prenom)
 			{
 				reponse = 1;
+				eleve = i;
+				break;
+			}
+			else if( 0 == fiche[i].nom && 0 == fiche[i].prenom)
+			{
+				reponse = -2;
+				nouveauEleve = i;
+				break;
 			}
 			else
 			{
 				reponse = -1;
 			}
 		}
+
 		if (reponse == 1)
 		{
 			printf_s("Combient de note a saisire ? \n");
 			scanf_s("%d", &nombre);
-			total = nombre + fiche[i].nbnotes;
+			total = nombre + fiche[eleve].nbnotes;
 			if (total<=50)
 			{
+				
 				printf_s("Entree les valeur \n");
-				for (int j = fiche[i].nbnotes; j < total; j++)
+				for (int j = fiche[eleve].nbnotes; j < total; j++)
 				{
-					scanf_s("%f", &fiche->note[i]);
+					scanf_s("%f", &fiche->note[eleve]);
 				}
-				fiche->nbnotes = total;
 				return 0;
+				fiche->nbnotes = total;
 			}
 			else{
 				return -1;
@@ -72,19 +86,17 @@ int Saisir(noteseninfo* fiche, int* index)
 		}
 		else
 		{
-			*index++;//pour verouiller le nouvelle emplacement
-			j = *index;
-			strcpy_s(fiche[j].nom, nom);//pour copier les deux tableau
-			strcpy_s(fiche[j].prenom,prenom );//pour copier les deux tableau
+			strcpy_s(fiche[nouveauEleve].nom, nom);//pour copier les deux tableau
+			strcpy_s(fiche[nouveauEleve].prenom,prenom );//pour copier les deux tableau
 			printf_s("Combient de note a saisire ? \n");
 			scanf_s("%d", &nombre);
 			total = nombre;
 			if (total <= 50)
 			{
 				printf_s("Entree les valeur \n");
-				for (int j = fiche[i].nbnotes; j < total; j++)
+				for (int j = fiche[nouveauEleve].nbnotes; j < total; j++)
 				{
-					scanf_s("%f", &fiche->note[i]);
+					scanf_s("%f", &fiche->note[eleve]);
 				}
 				fiche->nbnotes = total;
 				return 0;
@@ -94,12 +106,12 @@ int Saisir(noteseninfo* fiche, int* index)
 				return -1;
 			}
 		}
-	}
+
 }
 
 int main(int argc, char** argv)
 {
-	noteseninfo fiche[100];
+	noteseninfo fiche[100] = {0};
 	noteseninfo* dirFiche=fiche;
 	int chiffre = 0, retour = -1, index = 0;
 	int* indexP = &index;
@@ -114,6 +126,10 @@ int main(int argc, char** argv)
 		{
 		case 1: retour = Saisir(dirFiche,indexP); break;
 		case 2: affiche(dirFiche,index); break;
+		}
+		if (retour == -1)
+		{
+			printf_s("Erreur\n");
 		}
 
 	} while (chiffre != -1);
